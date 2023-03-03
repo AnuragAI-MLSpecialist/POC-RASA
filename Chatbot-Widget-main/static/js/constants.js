3795e717f3bce65c94adef51ec6a7eb69aae6ba1
Anurag Srivastava <126451923+AnuragAI-MLSpecialist@users.noreply.github.com>
2023-03-03 17:18:09 +0530

    Update constants.js

--- Chatbot-Widget-main/static/js/constants.js
+++ Chatbot-Widget-main/static/js/constants.js
@@ -1,3 +1,3 @@
 const action_name = "action_hello_world";
-const rasa_server_url = "http://3.85.33.254:5005/webhooks/rest/webhook";
+const rasa_server_url = "http://54.237.109.139:5005/webhooks/rest/webhook";
 const sender_id = uuidv4();
